e1b57964ce7a5fdac9bcbe96cba7a0f10ad1fe4a
Elizabeth <mwendemuthusi6@gmail.com>
2023-02-09 10:32:04 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+  def initialize (word)
+    @word = word
+  end
+  def match(arr)
+    found= arr.select {|word| word.chars.sort== @word.chars.sort}
+    found
+  end
+end
+
+a1= Anagram.new('listen')
+p a1.match(%w[enlists google inlets banana])
